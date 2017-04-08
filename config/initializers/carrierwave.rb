54ddd29e1100dd56fa353b130523abd6db634c1d
venkatgupta <venkat.gupta@pragmaedge.com>
2017-04-08 17:25:18 -0400

    images, carrier wave, s3, figaro

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,10 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              => 'AWS',                        # required
+    aws_access_key_id:     =>env['aws_access_key_id'],                        # required
+    aws_secret_access_key: =>env['aws_secret_access_key'],                        # required              
+  }
+  config.fog_directory  = ['fog_directory']            # required
+                                       
+end
